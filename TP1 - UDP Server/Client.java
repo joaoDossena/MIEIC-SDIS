91b453e9fcb6f771643be5183dfe1c604754b8a4
joaoDossena <joaopdossena@gmail.com>
2021-02-19 12:39:48 +0000

    Adding Server class

--- TP1 - UDP Server/Client.java
+++ TP1 - UDP Server/Client.java
@@ -13,16 +13,21 @@ public class Client {
 		InetAddress address = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]); 
 		byte[] sbuf = args[2].getBytes();
+		// byte[] sbuf = "Testing".getBytes();
 		// TODO: get <opnd>*
 
 		// send request
 		DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
 		socket.send(packet);
+        System.out.println("Sent request!");
+
 
 		// get response
         byte[] rbuf = new byte[sbuf.length];
         packet = new DatagramPacket(rbuf, rbuf.length);
         socket.receive(packet);
+        System.out.println("Received a response!");
+
 
         // display response
         String received = new String(packet.getData());
---
+++ TP1 - UDP Server/Server.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	public static void main(String[] args) throws IOException{
+		if(args.length != 1) {	//Review 
+			System.out.println("Usage: java Server <port number>");
+			return;
+		}
+
+		// opening socket
+		int port = Integer.parseInt(args[0]);
+		DatagramSocket serverSocket = new DatagramSocket(port);
+
+		// receiving request
+        byte[] rbuf = new byte[256];
+        DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);        
+        serverSocket.receive(packet);
+        String request = new String(packet.getData());
+        System.out.println("Request: " + request);
+	}
+}
