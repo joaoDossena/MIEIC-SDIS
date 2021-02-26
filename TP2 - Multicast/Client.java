3321e6f8713c2be477ef0acf3aefed1eb8c237f2
joaoDossena <joaopdossena@gmail.com>
2021-02-26 12:32:45 +0000

    Changing names

---
+++ TP2 - Multicast/Client.java
@@ -0,0 +1,53 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	public static void main(String[] args) throws IOException{
+		if(args.length != 4) {
+			System.out.println("Usage: java Client <multicast address> <multicast port> <oper> <opnd> *");
+			return;
+		}
+
+
+        InetAddress group = InetAddress.getByName(args[0]);
+		MulticastSocket socket = new MulticastSocket(Integer.parseInt(args[1]));
+        socket.joinGroup(group);
+        byte[] buf = new byte[256];
+
+		while (true) {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            String received = new String(
+              packet.getData(), 0, packet.getLength());
+            if ("end".equals(received)) {
+                break;
+            }
+        }
+        socket.leaveGroup(group);
+        socket.close();
+		// // prepare request
+		// DatagramSocket socket = new DatagramSocket(); //opening socket
+		// InetAddress address = InetAddress.getByName(args[0]);
+		// int port = Integer.parseInt(args[1]); 
+		// byte[] sbuf = args[2].getBytes();
+		// // TODO: get <opnd>*
+
+		// // send request
+		// DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
+		// socket.send(packet);
+  //       System.out.println("Sent request!");
+
+
+		// // get response
+  //       byte[] rbuf = new byte[sbuf.length];
+  //       packet = new DatagramPacket(rbuf, rbuf.length);
+  //       socket.receive(packet);
+  //       System.out.println("Received a response!");
+
+
+  //       // display response
+  //       String received = new String(packet.getData());
+  //       System.out.println("Received Message: " + received);
+  //       socket.close();
+	}
+}
---
+++ TP2 - Multicast/Server.java
@@ -0,0 +1,43 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	public static void main(String[] args) throws IOException{
+		if(args.length != 3) {
+			System.out.println("Usage: java Server <port number> <multicast address> <multicast port>");
+			return;
+		}
+
+
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress group = InetAddress.getByName(args[1]);
+		byte[] buf = "Message".getBytes();
+		int multicastPort = Integer.parseInt(args[2]);
+
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, group, multicastPort);
+        socket.send(packet);
+        socket.close();
+
+
+
+		// // opening socket
+		// int port = Integer.parseInt(args[0]);
+		// DatagramSocket serverSocket = new DatagramSocket(port);
+
+		// // receiving request
+  //       byte[] rbuf = new byte[256];
+  //       DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);        
+  //       serverSocket.receive(packet);
+  //       String request = new String(packet.getData());
+  //       System.out.println("Request: " + request);
+
+  //       // prepare response
+        
+
+  //       // send response
+  //       InetAddress destAddress = packet.getAddress();
+  //       int destPort = packet.getPort();
+		// packet = new DatagramPacket(rbuf, rbuf.length, destAddress, destPort);
+		// serverSocket.send(packet);
+	}
+}
