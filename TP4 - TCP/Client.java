c828a1d23cda7e5f41f4017512c1e4fdd5b36cc2
joaoDossena <joaopdossena@gmail.com>
2021-03-12 11:39:42 +0000

    Example works

--- TP4 - TCP/Client.java
+++ TP4 - TCP/Client.java
@@ -1,3 +1,34 @@
+import java.net.*;
+import java.io.*;
+
 public class Client {
-	
+
+	public static void main(String[] args){
+		if(args.length != 4 && args.length != 5) {
+			System.out.println("Usage: java Client <host_name> <port_number> <oper> <opnd1> [<opnd2>]");
+			return;
+		}
+
+		String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+ 
+        try (Socket socket = new Socket(hostname, port)) {
+ 
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+            String time = reader.readLine();
+ 
+            System.out.println(time);
+ 
+ 
+        } catch (UnknownHostException ex) {
+ 
+            System.out.println("Server not found: " + ex.getMessage());
+ 
+        } catch (IOException ex) {
+ 
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+	}
 }
