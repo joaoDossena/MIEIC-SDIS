c70612ededa9ba1b6f9467d85447363ddb9c2442
joaoDossena <joaopdossena@gmail.com>
2021-04-24 19:52:33 +0100

    SSL working with no authentication

--- TP5 - JSSE/Client.java
+++ TP5 - JSSE/Client.java
@@ -1,9 +1,9 @@
-import java.io.*;
-
+import java.io.OutputStream;
+import java.util.Arrays;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
-
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class Client {
@@ -32,33 +32,57 @@ public class Client {
             return;
         }
 
+        //set the type of trust store
+        System.setProperty("javax.net.ssl.trustStoreType","JKS");
+        //set the password with which the truststore is encripted
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+        //set the name of the trust store containing the server's public key and certificate           
+        System.setProperty("javax.net.ssl.trustStore", "./truststore");
+        //set the password with which the client keystore is encripted
+        System.setProperty("javax.net.ssl.keyStorePassword","123456");
+        //set the name of the keystore containing the client's private and public keys
+        System.setProperty("javax.net.ssl.keyStore","./client.keys");
+
         SSLSocketFactory factory = null;  
          
         factory = (SSLSocketFactory) SSLSocketFactory.getDefault();  
  
         try (SSLSocket socket = (SSLSocket) factory.createSocket(hostname, port)) {
- 
+
             OutputStream output = socket.getOutputStream();
             String request = operation + " " + dnsName;
             if(operation.equals("REGISTER")){
                 request += " " + args[4];
-                if(args.length == 6){
-                    request += " " + args[5];
+                if(args.length > 5){
+                    String[] cypherSuites = Arrays.copyOfRange(args, 5, args.length);
+                    try {
+                        socket.setEnabledCipherSuites(cypherSuites);
+                        System.out.println("Suite: " + cypherSuites[0]);
+                    } catch (Exception e) {
+                        System.out.println("Client: Unknown cypher suite. Exiting...");
+                        e.printStackTrace();
+                        return;
+                    }
                 }
             }
-            else if(args.length == 5){
-                request += " " + args[4];
+            else if(args.length > 4){
+                String[] cypherSuites = Arrays.copyOfRange(args, 4, args.length);
+                try {
+                    socket.setEnabledCipherSuites(cypherSuites);
+                } catch (Exception e) {
+                    System.out.println("Client: Unknown cypher suite. Exiting...");
+                    e.printStackTrace();
+                    return;
+                }
             }
-
+            socket.startHandshake();
             output.write(request.getBytes());
-  
+            socket.close();
         } catch (UnknownHostException ex) {
- 
             System.out.println("Client: SSLServer not found: " + ex.getMessage());
- 
-        } catch (IOException ex) {
- 
-            System.out.println("I/O error: " + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Client: " + ex.getMessage());
+            ex.printStackTrace();
         }
 	}
 
--- TP5 - JSSE/SSLServer.java
+++ TP5 - JSSE/SSLServer.java
@@ -1,9 +1,8 @@
 import java.io.InputStream;
-
+import java.util.Arrays;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
-
 
 import java.io.IOException;
 
@@ -11,33 +10,46 @@ public class SSLServer {
 	private static DNSServer dns = new DNSServer();
 
 	public static void main(String[] args){
-		if(args.length < 1 || args.length > 2) {
+		if(args.length < 1) {
 			System.out.println("Usage: java SSLServer <port> <cypher-suite>*");
 			return;
 		}
 
+		//set the type of trust store
+        System.setProperty("javax.net.ssl.trustStoreType","JKS");
+        //set the password with which the truststore is encripted
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+        //set the name of the trust store containing the client public key and certificate
+        System.setProperty("javax.net.ssl.trustStore", "./truststore");
+        //set the password with which the server keystore is encripted
+        System.setProperty("javax.net.ssl.keyStorePassword","123456");
+        //set the name of the keystore containing the server's private and public keys
+        System.setProperty("javax.net.ssl.keyStore","./server.keys");
+
 		int port = Integer.parseInt(args[0]);
-		String cypherSuite = "";
-		if(args.length == 2)
-			cypherSuite = args[1];
-		else
-			cypherSuite = "default";
 
 		SSLServerSocketFactory factory = null;  
 		 
 		factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();  
  
         try (SSLServerSocket serverSocket = (SSLServerSocket) factory.createServerSocket(port)) {
+			if(args.length > 1){
+	        	String[] cypherSuites = Arrays.copyOfRange(args, 1, args.length);
+				try { 
+	                serverSocket.setEnabledCipherSuites(cypherSuites);
+                    System.out.println("Suite: " + cypherSuites[0]);
+	            } catch (Exception e) {
+					System.out.println("SSLServer: Unknown cypher suite. Exiting...");
+	                e.printStackTrace();
+	                return;
+	            }
+			}
  
             System.out.println("SSLServer is listening on port " + port);
  
             while (true) {
                 SSLSocket socket = (SSLSocket) serverSocket.accept();
                 System.out.println("SSLServer: New client connected");
-                // Require client authentication  
-				socket.setNeedClientAuth(true); 
-                System.out.println("SSLServer: Required client authentication");
-
  
                 InputStream input = socket.getInputStream();
                 byte[] buf = new byte[256];
@@ -46,8 +58,6 @@ public class SSLServer {
 
                 String received = new String(buf);
 	            handleRequest(received);
-
- 
             }
  
         } catch (IOException ex) {
